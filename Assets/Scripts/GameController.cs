bba8775b7318d8c46f08f036c68f5344340c76f5
GreteO <ojagre@tlu.ee>
2019-04-19 22:11:16 +0300

    Initial commit and added assets

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,185 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+
+    public bool isMusicOn;
+    public bool isGameStartedFirstTime;
+
+    public int highScore;
+    public int currentScore;
+    public int currentLevel;
+
+    private GameData data;
+
+    void Awake()
+    {
+        CreateInstance();
+        InitializeGameVariables();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void CreateInstance()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void InitializeGameVariables()
+    {
+        Load();
+
+        if (data != null)
+        {
+            isGameStartedFirstTime = data.GetIsGameStartedFirstTime();
+        }
+        else
+        {
+            isGameStartedFirstTime = true;
+        }
+
+        if (isGameStartedFirstTime)
+        {
+            isMusicOn = true;
+            isGameStartedFirstTime = false;
+            highScore = 0;
+
+            data = new GameData();
+            data.SetIsMusicOn(isMusicOn);
+            data.SetIsGameStartedFirstTime(isGameStartedFirstTime);
+            data.SetHighScore(highScore);
+
+            Save();
+
+            Load();
+        }
+        else
+        {
+            highScore = data.GetHighScore();
+            isGameStartedFirstTime = data.GetIsGameStartedFirstTime();
+            isMusicOn = data.GetIsMusicOn();
+        }
+
+    }
+
+
+    public void Save()
+    {
+        FileStream file = null;
+
+        try
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            file = File.Create(Application.persistentDataPath + "/save.dat");
+
+            if (data != null)
+            {
+                data.SetHighScore(highScore);
+                data.SetIsGameStartedFirstTime(isGameStartedFirstTime);
+                data.SetIsMusicOn(isMusicOn);
+                bf.Serialize(file, data);
+            }
+
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e, this);
+
+        }
+        finally
+        {
+            if (file != null)
+            {
+                file.Close();
+            }
+        }
+    }
+
+    public void Load()
+    {
+        FileStream file = null;
+
+        try
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            file = File.Open(Application.persistentDataPath + "/save.dat", FileMode.Open);
+            data = bf.Deserialize(file) as GameData;
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e, this);
+        }
+        finally
+        {
+            if (file != null)
+            {
+                file.Close();
+            }
+        }
+    }
+
+
+    [Serializable]
+    class GameData
+    {
+        private bool isGameStartedFirstTime;
+        private bool isMusicOn;
+
+        private int highScore;
+
+        public void SetIsGameStartedFirstTime(bool isGameStartedFirstTime)
+        {
+            this.isGameStartedFirstTime = isGameStartedFirstTime;
+        }
+
+        public bool GetIsGameStartedFirstTime()
+        {
+            return this.isGameStartedFirstTime;
+        }
+
+        public void SetIsMusicOn(bool isMusicOn)
+        {
+            this.isMusicOn = isMusicOn;
+        }
+
+        public bool GetIsMusicOn()
+        {
+            return this.isMusicOn;
+        }
+
+        public void SetHighScore(int highScore)
+        {
+            this.highScore = highScore;
+        }
+
+        public int GetHighScore()
+        {
+            return this.highScore;
+        }
+    }
+
+
+    // Update is called once per frame
+//    void Update()
+  //  {
+        
+    //}
+}
---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+
+    public static MusicController instance;
+
+    public AudioClip[] audioClips;
+
+    [HideInInspector]
+    public AudioSource audioSource;
+    // Start is called before the first frame update
+
+
+    /*void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+
+    void Awake()
+    {
+        CreateInstance();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void CreateInstance()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void PlayBgMusic()
+    {
+        if (!audioSource.isPlaying)
+        {
+            AudioClip bgMusic = audioClips[0];
+            if (bgMusic)
+            {
+                audioSource.clip = bgMusic;
+                audioSource.loop = true;
+                audioSource.volume = 0.6f;
+                audioSource.Play();
+            }
+        }
+    }
+
+    public void GameplaySound()
+    {
+        if (!audioSource.isPlaying)
+        {
+            AudioClip gameplaySound = audioClips[1];
+            if (gameplaySound)
+            {
+                audioSource.clip = gameplaySound;
+                audioSource.loop = true;
+                audioSource.volume = 0.6f;
+                audioSource.Play();
+            }
+        }
+    }
+
+    public void StopAllSound()
+    {
+        if (audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+    }
+}
